356163a6c40f3c6258d2a11e9d8b6654db7da19d
U82146510 <U82146510@protonmail.com>
2025-07-27 09:10:57 +0100

    creating seed function

---
+++ src/bot/utils/seed.ts
@@ -0,0 +1,13 @@
+import {Auth} from '../../models/adminPanel.ts';
+
+export async function creteAdmin():Promise<void>{
+    try {
+        const user = await Auth.findOne({username:'admin'});
+        if(!user){
+            await Auth.create({username:'admin',password:'aA123456789!@#'})
+            return;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
